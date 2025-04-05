c70eb72118464903236756ecc5cb473355b42700
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-04-05 18:18:01 -0500

    rename top-level controller

--- src/tars.py
+++ src/tars.py
@@ -1,36 +1,46 @@
-from speech_controller import SpeechController
+from listen_controller import ListenController
 from convo_controller import ConvoController
 from tts_controller import TtsController
 from personality_parameters import PersonalityParameters
+import logging
 
-class Controller:
+class TARS:
     def __init__(self, env_path: str = "../.env"):
-        self.speech_controller = SpeechController(env_path=env_path)
+        # Configure logging
+        self.logger = logging.getLogger('tars')
+        self.logger.info("Initializing TARS...")
+        
+        # Initailize controllers
+        self.listen_controller = ListenController(env_path=env_path)
         self.convo_controller = ConvoController(env_path=env_path)
         self.tts_controller = TtsController(env_path=env_path)
         self.personality_parameters = PersonalityParameters()
+        
+        # Log the initialization
+        self.logger.info("TARS initialized successfully.")
     
     async def run(self):
         """Runs the program"""
+        self.logger.info("Beginning main runtime loop...")
         
         # Main runtime loop
         while True:
             # Wait for the wake phrase
-            detected = self.speech_controller.listen_for_wake_phrase()
+            detected = self.listen_controller.listen_for_wake_phrase()
             
             # Check if the transcript contains wake word(s)/phrase(s)
             if detected:
-                print("Wake phrase detected!")
+                self.logger.info("Wake phrase detected!")
             else:
-                print("Wake phrase not detected. Please try again.")
+                self.logger.warning("Wake phrase not detected. Please try again.")
                 continue
 
             # Listen for the command after the wake word has been detected
-            user_command = self.speech_controller.listen_for_command()
+            user_command = self.listen_controller.listen_for_command()
             
             # Check command validity
             if user_command is None:
-                print("No command detected. Please try again.")
+                self.logger.warning("No command detected. Please try again.")
                 continue
             
             # Generate a repsonse using Gemini
@@ -40,11 +50,10 @@ class Controller:
             )
             
             # Print the response
-            print("=== REPLY FROM TARS ===")
-            print(f'"{response}"')
-            print("=== END OF MESSAGE ===")
+            self.logger.info("=== REPLY FROM TARS ===")
+            self.logger.info(f'"{response}"')
+            self.logger.info("=== END OF MESSAGE ===")
             
             # Speak the response using TTS
             await self.tts_controller.speak(response, self.personality_parameters)
-            
             
