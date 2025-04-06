3476a807e079c30c9be7ad58c2616946d54bfd65
Raymann Singh <raymann.singh@gmail.com>
2025-04-05 20:46:50 -0500

    added loggin to servo_logic.py

--- src/modules/helpers/servo_logic.py
+++ src/modules/helpers/servo_logic.py
@@ -19,6 +19,12 @@ import sys
 import logging
 
 # Configure logging
+logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[logging.StreamHandler()]
+    )
+
 logger = logging.getLogger('servo_controller')
 
 # Try to import the Adafruit PCA9685 library
