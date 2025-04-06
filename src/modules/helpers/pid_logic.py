2bee081a72ce73a6e4215f781a674604647c0f2c
Raymann Singh <raymann.singh@gmail.com>
2025-04-06 05:43:17 -0500

    mroe outputs

--- src/modules/helpers/pid_logic.py
+++ src/modules/helpers/pid_logic.py
@@ -5,7 +5,9 @@ class PID:
         self.kp = kp
         self.ki = ki
         self.kd = kd
+        self.proportional = 0
         self.integral = 0
+        self.derivative = 0
         self.prev_error = 0
         self.output_limits = output_limits
 
@@ -14,12 +16,15 @@ class PID:
         self.integral += error * dt
 
         # Anti-windup clamp
-        self.integral = max(min(self.integral, 100), -100)
+        self.integral = self.ki * max(min(self.integral, 100), -100)
 
-        derivative = (error - self.prev_error) / dt if dt > 0 else 0
+        der = (error - self.prev_error) / dt if dt > 0 else 0
+        self.derivative = (self.kd * der)
         self.prev_error = error
 
-        output = (self.kp * error) + (self.ki * self.integral) + (self.kd * derivative)
+        self.proportional = (self.kp * error)
+
+        output = self.proportional + self.integral + self.derivative
 
         # Clamp output
         return max(min(output, self.output_limits[1]), self.output_limits[0])
