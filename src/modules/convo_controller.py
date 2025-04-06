8b99793c9392ac953c26e210d69a77b5b7aa839a
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-04-05 22:54:24 -0500

    add basic function calling

--- src/modules/convo_controller.py
+++ src/modules/convo_controller.py
@@ -3,16 +3,22 @@ from google.genai import types
 from modules.models.personality_parameters import PersonalityParameters
 import dotenv, logging
 
-class ConvoController():
-    """Controller for conversation generation using the Gemini API"""
+AUTO_FUNCTION_CALLING = False
 
-    def __init__(self, env_path: str = "../.env"):
+class ConvoController():
+    """
+    Controller for conversation generation using the Gemini API
+    """
+
+    def __init__(self, env_path: str = "../.env", function_declarations: list = []):
         # Initialize logger
         self.logger = logging.getLogger('convo_controller')
+        self.logger.info("Initializing ConvoController...")
         
         # Initialize API 
         self.api_key = dotenv.get_key(dotenv_path=env_path, key_to_get="GEMINI_API_KEY")
         self.model = "gemini-2.0-flash"
+        self.function_declarations = function_declarations
         
         # Source: the TARS/Interstellar Fandom page (https://interstellarfilm.fandom.com/wiki/TARS)
         self.backstory = """"TARS is one of four former U.S. Marine Corps tactical robots along with PLEX, CASE and KIPP featured in the Interstellar universe. He is one of the crew members of Endurance along with Cooper, Brand, Doyle, Romilly, and CASE. TARS' personality can be characterized as witty, sarcastic, and humorous, traits programmed into him to make him a better suited companion. TARS also appears to be somewhat more versatile than CASE, being suited for tasks ranging from piloting to data collection.
@@ -53,38 +59,125 @@ class ConvoController():
         {self.examples}"""
         
         # Conversation memory
-        self.memory = [f"SYSTEM: {self.system_prompt}"]
+        self.memory: list[types.Content] = []
         
         # Load API clients
         self.client = genai.Client(api_key=self.api_key)
+        self.tools = types.Tool(function_declarations=self.function_declarations)
         
+        # Check for function declarations and set up the config accordingly
+        if len(function_declarations) != 0:
+            self.tools = types.Tool(function_declarations=function_declarations)
+            self.config = types.GenerateContentConfig(
+                tools=[self.tools],
+                system_instruction=self.system_prompt,
+                temperature=2.0,
+                automatic_function_calling=types.AutomaticFunctionCallingConfig(disable=not AUTO_FUNCTION_CALLING))
+        else:
+            self.config = types.GenerateContentConfig(
+                system_instruction=self.system_prompt,
+                temperature=2.0,
+                automatic_function_calling=types.AutomaticFunctionCallingConfig(disable=not AUTO_FUNCTION_CALLING))
+        
+        # Log
         self.logger.info("ConvoController initialized successfully.")
+    
+    def set_function_declarations(self, function_declarations: list):
+        """
+        Sets the function declarations for the conversation model.
+        """
+        self.function_declarations = function_declarations
+        
+        if len(function_declarations) != 0:
+            self.tools = types.Tool(function_declarations=function_declarations)
+            self.config = types.GenerateContentConfig(
+                tools=[self.tools],
+                system_instruction=self.system_prompt,
+                temperature=2.0,
+                automatic_function_calling=types.AutomaticFunctionCallingConfig(disable=not AUTO_FUNCTION_CALLING))
+        else:
+            self.config = types.GenerateContentConfig(
+                system_instruction=self.system_prompt,
+                temperature=2.0,
+                automatic_function_calling=types.AutomaticFunctionCallingConfig(disable=not AUTO_FUNCTION_CALLING))
+        
+        self.logger.info("Function declarations set successfully.")
         
     def reset_memory(self):
-        """Resets the conversation memory."""
-        self.memory = [f"SYSTEM: {self.system_prompt}"]
+        """
+        Resets the conversation memory.
+        """
+        self.memory = []
         self.logger.info("Conversation memory reset.")
 
-    def send_message(self, msg: str, personality_parameters: PersonalityParameters = None) -> str:
+    def send_message(self, msg: str, personality_parameters: PersonalityParameters = None) -> tuple[str, types.FunctionCall]:
         """
         Sends a message for TARS to respond to.
         
         @returns str - The response to the sent message
         """
-        _contents = f"PREVIOUS CONVERSATION: {self.memory}" 
             
+        message_text = ""
         if personality_parameters is not None:
-            _contents += f"\nPERSONALITY PARAMETERS: {personality_parameters}"
+            message_text += f"CURRENT PERSONALITY PARAMETERS: {personality_parameters}\n"
             
-        _contents += f"\nNEW USER MESSAGE: '{msg}'"
+        message_text += f"NEW USER MESSAGE: '{msg}'"
+        
+        user_contents = [
+            types.Content(
+                role = "user",
+                parts=[types.Part(text=message_text)]
+            )
+        ]
         
         response = self.client.models.generate_content(
             model=self.model,
-            contents=_contents
+            contents=user_contents,
+            config=self.config
         )
         
         # Update conversation memory with the new message and response
-        self.memory.append(f"User: {msg}")
-        self.memory.append(f"TARS: {response.text}")
+        self.memory.append(user_contents)
+        
+        ai_contents = [
+            types.Content(
+                role = "model",
+                parts=[types.Part(text=response.text)]
+            )
+        ]
+        
+        self.memory.append(ai_contents)
+        
+        return (response.text, response.function_calls[0] if response.function_calls is not None else None)
+    
+    def send_function_result(self, function_call: types.FunctionCall, function_result: any) -> str:
+        """
+        Sends the result of a function call back to TARS.
+        
+        @returns str - The response to the sent message
+        """
+        
+        # Create a new content object for the function result
+        function_result_content = types.Part.from_function_response(
+            name=function_call.name,
+            response={"result": function_result},
+        )
+        
+        # Append the function call and function result to the conversation memory
+        function_call_message = types.Content(role="model", parts=[types.Part(function_call=function_call)])
+        function_call_result = types.Content(role="user", parts=[function_result_content])
+        
+        # Generate a response based on the function result
+        response = self.client.models.generate_content(
+            model=self.model,
+            contents=[function_call_message, function_call_result],
+            config=self.config
+        )
+        
+        # Update conversation memory with the new message and response
+        self.memory.append(function_call_message)
+        self.memory.append(function_call_result)
+        self.memory.append(response)
         
         return response.text
+        
---
+++ src/modules/tars_tools.py
@@ -0,0 +1,12 @@
+class TarsTools():
+    """
+    TarsTools is a class that provides methods to interact with Tars.
+    """
+
+    @staticmethod
+    def get_weather():
+        """
+        Gets the weather information.
+        """
+        # Placeholder for actual weather retrieval logic
+        return "The weather is cloudy with a high of 54°F." # TODO: Replace with actual weather retrieval logic
